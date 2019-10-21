6660ec9f7e26de06dd0a7f81073d6bd81f4c8692
RedIce-X <raunaknaik99@gmail.com>
2019-10-22 00:49:03 +0530

    Login page only

---
+++ DBMS Proj/HotelManagement/HotelManagement/Login.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HotelManagement
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.userIDLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.userIDTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userIDLabel
+            // 
+            this.userIDLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.userIDLabel.AutoSize = true;
+            this.userIDLabel.BackColor = System.Drawing.Color.Transparent;
+            this.userIDLabel.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userIDLabel.ForeColor = System.Drawing.Color.Black;
+            this.userIDLabel.Location = new System.Drawing.Point(126, 139);
+            this.userIDLabel.Name = "userIDLabel";
+            this.userIDLabel.Size = new System.Drawing.Size(87, 21);
+            this.userIDLabel.TabIndex = 2;
+            this.userIDLabel.Text = "User ID :";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.passwordLabel.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLabel.ForeColor = System.Drawing.Color.Black;
+            this.passwordLabel.Location = new System.Drawing.Point(107, 191);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(106, 21);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password :";
+            this.passwordLabel.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.LoginBtn.AutoSize = true;
+            this.LoginBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.LoginBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LoginBtn.BackgroundImage")));
+            this.LoginBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.LoginBtn.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.LoginBtn.Location = new System.Drawing.Point(324, 280);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(170, 44);
+            this.LoginBtn.TabIndex = 5;
+            this.LoginBtn.Text = "Login";
+            this.LoginBtn.UseVisualStyleBackColor = false;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // userIDTextBox
+            // 
+            this.userIDTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.userIDTextBox.Location = new System.Drawing.Point(242, 136);
+            this.userIDTextBox.Multiline = true;
+            this.userIDTextBox.Name = "userIDTextBox";
+            this.userIDTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.userIDTextBox.Size = new System.Drawing.Size(380, 24);
+            this.userIDTextBox.TabIndex = 6;
+            this.userIDTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.userIDTextBox.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.passwordTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.passwordTextBox.Location = new System.Drawing.Point(242, 194);
+            this.passwordTextBox.Multiline = true;
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(380, 21);
+            this.passwordTextBox.TabIndex = 7;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.userIDTextBox);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userIDLabel);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userIDLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Button LoginBtn;
+        private System.Windows.Forms.TextBox userIDTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+    }
+}
+
